d9ce4c08e562312aab2b8dceab6dacc331d9d4b8
Rameez-Raja-1999 <rameez1098raja@gmail.com>
2026-04-02 22:15:48 +0530

    js code for adding functionality

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const inputBox = document.querySelector(".input-box");
+const inputBox= document.querySelector("#input-box");
+const listContainer= document.querySelector("#list-container");
+
+function addTask(){
+    if(inputBox.value===""){
+        alert("you must have to write something");
+    }else{
+        const li = document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+    }
+
+    inputBox.value="";
+}
