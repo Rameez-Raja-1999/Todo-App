a307e99a0108f0dd840459441e99c9a6be6b78d3
Rameez-Raja-1999 <rameez1098raja@gmail.com>
2026-04-03 17:34:55 +0530

    Removing css comments

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ function addTask(){
         const li = document.createElement("li");
         li.innerHTML=inputBox.value;
         listContainer.appendChild(li);
-    }
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+        }
 
     inputBox.value="";
 }
